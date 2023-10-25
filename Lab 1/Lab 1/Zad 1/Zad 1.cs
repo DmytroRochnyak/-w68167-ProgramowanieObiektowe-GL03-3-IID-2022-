4f91dcd2c1b5f81ea3c22cca9d2ca7e11738559a
w68167 <w68167@RA117A-15>
2023-10-25 13:55:48 +0200

    Lab 1

---
+++ Lab 1/Lab 1/Zad 1/Zad 1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Podaj liczbę całkowitą: ");
+        int liczba;
+
+        if (int.TryParse(Console.ReadLine(), out liczba))
+        {
+            if (liczba % 2 == 0)
+            {
+                Console.WriteLine("Podana liczba jest parzysta.");
+            }
+            else
+            {
+                Console.WriteLine("Podana liczba jest nieparzysta.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("To nie jest liczba całkowita.");
+        }
+    }
+}
